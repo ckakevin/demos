430374945f8e32820504b5637506d52091a06d48
shuh534 <54824444+shuh534@users.noreply.github.com>
2019-10-08 10:05:33 -0400

    Add files via upload

---
+++ week7/js/letter-a.js
@@ -0,0 +1,7 @@
+var a = document.getElementById("A");
+a.addEventListener("click", a_OnClick);
+
+function a_OnClick() {
+	var body = document.getElementById("body");
+	body.style.backgroundColor = "#45823e";
+}
